b47e23e32fbd2b867f87e250cd2a7e52d3120087
Jonas Kavaliauskas <jonas.kavaliauskas@yale.edu>
2020-05-22 08:31:20 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,8 +6,9 @@ def key_for_min_value(name_hash)
   name_hash.each do |key, value|
     if value < min_value
       min_value = value
+      return min_value
     end
-    return min_value
+    
   end
 
 end
